f35ede1e2270521b9bd7bec40934fdd409d2b179
Vuonggithubb <fanvuong99@gmail.com>
2023-07-07 12:58:48 +0700

    start fronend

---
+++ src/containers/System/ModalEditUser.js
@@ -0,0 +1,146 @@
+import React, {Component} from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { emitter } from "../../utils/emitter"
+
+class ModalEditUser extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            firstName: '',
+            lastName: '',
+            address: '',
+        }
+    }
+
+    
+    componentDidMount() {
+
+    }
+
+    toggle = () => {
+        this.props.toggleFromParent();
+    }
+
+    handleOnChageInput = (event, id) => {
+        let copyState = { ...this.state};
+        copyState[id] = event.target.value;
+        this.setState({
+            ...copyState
+        });
+
+    }
+
+    checkValidateInput = () => {
+        let isValid = true;
+        let arrInput = ['email', 'password', 'firstName', 'lastName', 'address'];
+        for (let i = 0; i< arrInput.length; i++){
+            if(!this.state[arrInput[i]]){
+                isValid = false;
+                alert('missing parameter:' + arrInput[i]);
+                break;
+            }
+        }
+        return isValid;
+    }
+
+    handleAddNewUser = () => {
+        let isValid = this.checkValidateInput();
+        if (isValid === true) {
+            this.props.createNewUser(this.state, 'abc');
+
+        }
+    } 
+    
+    render(){
+        return (
+            <Modal
+                isOpen={this.props.isOpen}
+                toggle={() => {this.toggle() }}
+                className={'modal-user-container'}
+                size='lg'
+            >
+                <ModalHeader toggle={() => {this.toggle()}}>Edit a user </ModalHeader>
+                <ModalBody>
+                    <div className="modal-user-body">
+                        <div className="input-container">
+                            <lable>Email</lable>
+                            <input
+                                type="text"
+                                onChange={(event) => {this.handleOnChageInput(event,'email' )}}
+                                value={this.state.email}
+                            ></input>
+                        </div>
+
+                        <div className="input-container">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                onChange={(event) => {this.handleOnChageInput(event, 'password')}}
+                                value={this.state.password}
+                            />
+                        </div> 
+
+                        <div className="input-container">
+                            <lable>First name</lable>
+                            <input
+                                type="text"
+                                onChange={(event) => {this.handleOnChageInput(event, 'firstName')}}
+                                value={this.state.firstName}
+                            />
+                        </div>  
+
+                        <div className="input-container">
+                            <label>Last name</label>
+                            <input
+                                type="text"
+                                onChange={(event) => {this.handleOnChageInput(event, 'lastName')}}
+                                value={this.state.lastName}
+                            />
+
+                        </div>
+
+                        <div className="input-container">
+                            <label>address</label>
+                            <input
+                                type="text"
+                                onChange={(event) => {this.handleOnChageInput(event, 'address')}}
+                                value={this.state.address}
+                            />
+
+                        </div>
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <Button
+                        color="primary"
+                        className="px-3"
+                        onClick={() =>{ this.handleAddNewUser()}}
+                    
+                    >Add new</Button>{' '}
+                    <Button color="secondary" className="px-3" onClick={() => { this.toggle()}}>Close</Button>
+                </ModalFooter>
+
+            </Modal>
+                
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = dispatch =>
+ {
+    return{
+
+    };
+ };
+
+ export default connect(mapStateToProps, mapDispatchToProps)(ModalEditUser);
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
-import { getAllUsers } from '../../services/userService';
+import { getAllUsers, createNewUserService, deleteUserService } from '../../services/userService';
+import ModalUser from './ModalUser';
+import ModalEditUser from './ModalEditUser';
+import { emitter} from '../../utils/emitter';
+
 class UserManage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false,
+            isOpenModalEditUser: false,
         }
     }
 
    
 
     async componentDidMount() {
+        await this.getAllUsersFromReact();
+    }
+
+    getAllUsersFromReact = async () => {
         let response = await getAllUsers('All');
         if (response && response.errCode === 0) {
             this.setState({
@@ -24,37 +34,113 @@ class UserManage extends Component {
 
     }
 
+    handleAddNewModal = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
+
+    createNewUser = async (data) => {
+        try { 
+            let response =await createNewUserService(data);
+            if(response && response.errCode !== 0) {
+                alert(response.errMessage)
+            }else{
+                await this.getAllUsersFromReact();
+                this.setState({
+                    isOpenModalUser: false
+                })
+
+                emitter.emit('EVENT_CLEAR_MODAL_DATA')
+            }
+            
+        } catch (e) {
+            console.log(e);
+            
+        }
+    }
+
+    handleDeleteUser = async (user) => {
+        console.log('click delete', user)
+        try {
+            let res = await deleteUserService(user.id);
+            if (res && res.errCode ===0){
+                await this.getAllUsersFromReact();
+            }
+            else 
+            {
+                alert(res.errMessage)
+            }
+        }catch(e){
+            console.log(e);
+        }
+    }
+
+    handleEditUser = (user) => {
+        console.log(user);
+        this.setState({
+            isOpenModalEditUser: true
+        })
+    }
+
 
     render() {
         let arrUsers = this.state.arrUsers;
+        console.log(arrUsers)
         return (
             <div className='users-container'>
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent= {this.toggleUserModal}
+                    createNewUser= {this.createNewUser}
+                />
+                <ModalEditUser
+                    isOpen={this.state.isOpenModalEditUser}
+                    // toggleFromParent= {this.toggleUserModal}
+                    // createNewUser= {this.createNewUser}
+                />
                 <div className="title text-center">Manage users</div>
+                <div className='mx-1'>
+                    <button
+                        className='btn btn-primary px-3'
+                        onClick={() => this.handleAddNewModal()}
+                    
+                    ><i className='fas fa-plus'></i> Add new users</button>
+                </div>
+
                 <div className='user-table mt-3 mx-1'>
                     <table id='customers'>
-                        <tr>
-                            <th>Email</th>
-                            <th>First name</th>
-                            <th>Last name</th>
-                            <th>Address</th>
-                            <th>Actions</th>
-                        </tr>
+                       
+                            <tr>
+                                <th>Email</th>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>Address</th>
+                                <th>Actions</th>
+                            </tr>
 
-                        {arrUsers && arrUsers.map((item, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>{item.email}</td>
-                                    <td>{item.firstName}</td>
-                                    <td>{item.lastName}</td>
-                                    <td>{item.address}</td>
-                                    <td>
-                                        <button className='btn-edit'><i className='fas fa-pencil-alt'></i></button>
-                                        <button className='btn-delete'><i className='fas fa-trash'></i></button>
-                                    </td>
-                                </tr>
+                            {arrUsers && arrUsers.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit' onClick={() => this.handleEditUser()}><i className='fas fa-pencil-alt'></i></button>
+                                            <button className='btn-delete' onClick={() => this.handleDeleteUser(item)}><i className='fas fa-trash'></i></button>
+                                        </td>
+                                    </tr>
 
-                            )
-                        })}
+                                )
+                            })}
+                        
                     </table>
                 </div>
 
